4628fba9ff796c4762e7bff333efbc604d8b74c2
Dishant1901 <dishantsinghom@gmail.com>
2023-02-26 23:47:52 +0530

    started working on  HeroCarousel

--- src/components/HeroCarousel/HeroCarousel.component.jsx
+++ src/components/HeroCarousel/HeroCarousel.component.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React ,{useState} from 'react'
+import HeroSlider from 'react-slick';
 
 const HeroCarousel = () => {
+  const [images ,setImages]= useState([]);
+ 
   return (
-    <div>HeroCarousel</div>
+    <>
+      <div className='lg:hidden'>
+        <HeroSlider>
+          {
+            images.map((images)=>(
+              <div className='w-full h-56 md:h-80 py-3'>
+                <img src=""
+                 alt=" Hero banner"
+                 className='w-full h-full rounded-md object-cover'
+                  />
+              </div>
+            ))
+          }
+        </HeroSlider>
+
+      </div>
+      <div className='hidden lg:block'>
+      <HeroSlider>
+          {
+            images.map((images)=>(
+              <div className='w-full h-96 px-2 py-3'>
+                <img src=""
+                 alt=" Hero banner"
+                 className='w-full h-full rounded-md object-cover'
+                  />
+              </div>
+            ))
+          }
+        </HeroSlider>
+      </div>
+    
+    </>
   )
 }
 
