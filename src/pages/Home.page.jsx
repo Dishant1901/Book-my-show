4a55b7c9cacca5caa5909b2bd5480a3707920e3b
Dishant1901 <dishantsinghom@gmail.com>
2023-02-26 21:49:59 +0530

    started working with tailwind

--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -8,33 +8,62 @@ import DefaultLayoutHoc from '../layout/Default.layout';
 
 
 const HomePage = () => {
-  const[recommendedMovies, setRecommendedMovies]= useState([]);
-  const[premierMovies,setPremierMovies]= useState([]);
-  const[onlineStreamEvent,setOnlineStreamEvent]= useState([]);
+  const [recommendedMovies, setRecommendedMovies] = useState([]);
+  const [premierMovies, setPremierMovies] = useState([]);
+  const [onlineStreamEvent, setOnlineStreamEvent] = useState([]);
 
-  return  (
+  return (
     <>
-      <HeroCarousel/>
+      <HeroCarousel />
 
       <div className='container mx-auto px-4 md:px-12 my-8'>
         <h1 className='text-2xl font-bold text-gray-800 sm:ml-3 ml-0 my-3'>
-           The best of Entertainment</h1>
-           <EntertainmentCard/>
+          The best of Entertainment</h1>
+        <EntertainmentCard />
       </div>
 
       <div className='container mx-auto px-4 md:px-12 my-8'>
-        <PosterSlider 
-        title = "Recommended Movies"
-        subject = "List of recommended movies"
-        posters = {recommendedMovies}
-        isDark = {false} />
+        <PosterSlider
+          title="Recommended Movies"
+          subject="List of recommended movies"
+          posters={recommendedMovies}
+          isDark={false}
+        />
       </div>
 
-      <div className='bg-p'></div>
-    
+      <div className='bg-premier-800 py-12'>
+        <div className=' container mx-auto px-4 md:px-12 my-8 flex flex-col gap-3'>
+          <div className='hidden md:flex'>
+            <img
+              src="https://in.bmscdn.com/discovery-catalog/collections/tr:w-1440,h-120/premiere-rupay-banner-web-collection-202104230555.png"
+              alt="Rupay"
+              className='w-full h-full'
+            />
+          </div>
+
+          <PosterSlider
+           title="Premiers"
+           subject="Brand new realse every Friday"
+           posters={premierMovies}
+           isDark={true}
+          />
+        </div>
+      </div>
+
+      <div className='container mx-auto px-4 md:px-12 my-8'>
+
+        <PosterSlider
+         title="Online streaming Events"
+         subject=""
+         posters={onlineStreamEvent}
+         isDark={false}
+        />
+
+      </div>
+
     </>
   )
-  
+
 };
 
 export default DefaultLayoutHoc(HomePage);
