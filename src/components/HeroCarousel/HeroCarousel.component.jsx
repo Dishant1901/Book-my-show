fdbf5f7aae0878eb925ba93f0c5e536a3aa2602e
Dishant1901 <dishantsinghom@gmail.com>
2023-03-02 09:33:14 +0530

    added slider

--- src/components/HeroCarousel/HeroCarousel.component.jsx
+++ src/components/HeroCarousel/HeroCarousel.component.jsx
@@ -1,17 +1,41 @@
 import React ,{useState} from 'react'
 import HeroSlider from 'react-slick';
+import { NextArrow, PrevArrow } from './Arrows.component';
 
 const HeroCarousel = () => {
   const [images ,setImages]= useState([]);
+
+  const settings ={
+    arrows:true,
+    slidesToShow:1,
+    infinite:true,
+    speed:500,
+    slideToScroll:1,
+    nextArrow: <NextArrow/> ,
+    prevArrow: <PrevArrow/> , 
+
+  };
+
+  const settingsLG={
+
+    arrows:true,
+    slidesToShow:1,
+    infinite:true,
+    speed:500,
+    slideToScroll:1,
+    nextArrow: < NextArrow/> ,
+    prevArrow:<PrevArrow/> , 
+
+  };
  
   return (
     <>
       <div className='lg:hidden'>
-        <HeroSlider>
+        <HeroSlider {...settings}>
           {
             images.map((images)=>(
               <div className='w-full h-56 md:h-80 py-3'>
-                <img src=""
+                <img src=''
                  alt=" Hero banner"
                  className='w-full h-full rounded-md object-cover'
                   />
@@ -22,11 +46,14 @@ const HeroCarousel = () => {
 
       </div>
       <div className='hidden lg:block'>
-      <HeroSlider>
+      {/* to show all slides */}
+      <HeroSlider {...settingsLG}>
+          {/* to use js in react use it inside {} */}
           {
             images.map((images)=>(
+              // thi is dyanamic array mapping
               <div className='w-full h-96 px-2 py-3'>
-                <img src=""
+                <img src=''
                  alt=" Hero banner"
                  className='w-full h-full rounded-md object-cover'
                   />
